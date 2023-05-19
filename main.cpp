e767f1be822fd0ff95398cf3584e08ba33a23e37
Poogal0 <golubchikov_2002@mail.ru>
2023-05-19 12:19:02 +0400

    v1

--- main.cpp
+++ main.cpp
@@ -4,26 +4,12 @@ class Car{
 		double x;
 		int p;
 		double v;
-		Car();
-		Car(int newP, double newV);
 };
 
-Car::Car() {
-	x = 0.0;
-	p = 1;
-	v = 0.0;
-}
-
-Car::Car(int newP, double newV) {;
-	x = 0.0;
-	p = newP;
-	v = newV;
-}
 int main () {
 	using namespace std;
 	cout << "Test v3.01:OK" << endl;
 	Car car;
-	Car carFord(5, 20);
 	cout << "car.x: " << car.x << endl;
 	cout << "car.p: " << car.p << endl;
 	cout << "car.v: " << car.v << endl;
@@ -33,8 +19,5 @@ int main () {
 	cout << "car.x: " << car.x << endl;
 	cout << "car.p: " << car.p << endl;
 	cout << "car.v: " << car.v << endl;
-	cout << "carFord.x: " << carFord.x << endl;
-	cout << "carFord.p: " << carFord.p << endl;
-	cout << "carFord.v: " << carFord.v << endl;
 	return 0;
 }
